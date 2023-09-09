7ad7c4c8717ca39f7aed2d8d7d8624aa970104cd
Tahmina-S <141042558+Tahmina-S@users.noreply.github.com>
2023-09-09 13:20:11 +0600

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -1,6 +1,15 @@
-def tahmina():
-    print("Name: Tahmina Sultana Sathi")
-    print("Adress: Commerce College Road,Mirpur#2")
-    print("Age: 26")
+def tahmina(n,a,nm,ag):
+    # print("Name: Tahmina Sultana Sathi")
+    # print("Adress: Commerce College Road,Mirpur#2")
+    # print("Age: 26")
+    print("Name: {} ".format(n))
+    print("Adress: {} ".format(a))
+    print("Number: {} ".format(nm))
+    print("Age: {} ".format(ag))
 
-tahmina()
+name=input("Name: ")
+address=input("Address: ")
+number=input("Number: ")
+age=input("Age: ")
+
+tahmina(name,address,number,age)
--- Q2.py
+++ Q2.py
@@ -1,24 +1,23 @@
-def min(l1):
-    min=l1[0]
-    for n in l1:
+def min(myNumber):
+    min=myNumber[0]
+    for n in myNumber:
         if n<min:
             min=n
     print("So, min num in the list is ", min)
-
-def max(l1):
-    max=l1[0]
-    for n in l1:
+    
+def max(myNumber):
+    max=myNumber[0]
+    for n in myNumber:
         if n>max:
             max=n
     print("So, max num in the list is ", max)
 
+digit=int(input("Enter your phone number's digit count limit:"))
+myNumber=[]
+for i in range(digit):
+    i=int(input("Enter {} digit : ".format(i+1)))
+    myNumber.append(i)
+print("Phone Number is: ",myNumber )
 
-print("Enter your phone number one by one digit:")
-l1=[]
-for i in range(11):
-    i=int(input())
-    l1.append(i)
-print("the number is: ", l1)
-
-min(l1)
-max(l1)
+min(myNumber)
+max(myNumber)
